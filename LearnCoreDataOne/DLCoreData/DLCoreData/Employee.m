a1f4de5ba2f124e4a183ea1b91b2e08f2c7a0402
Liqiankun <qiankunli@yahoo.com>
2016-03-18 08:37:08 +0800

    Add One

---
+++ LearnCoreDataOne/DLCoreData/DLCoreData/Employee.m
@@ -0,0 +1,15 @@
+//
+//  Employee.m
+//  LeranCoreData
+//
+//  Created by DavidLee on 16/3/16.
+//  Copyright © 2016年 DavidLee. All rights reserved.
+//
+
+#import "Employee.h"
+
+@implementation Employee
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
